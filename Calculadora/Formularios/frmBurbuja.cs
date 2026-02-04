3bfefb98609af4bed4b44bde927e15eb63ada365
angela <angeladlrdz@gmail.com>
2026-02-04 15:53:04 -0600

    Se crea un nuevo formulario para el registro, donde se ponen tabControl, ahi se ponen label, textbox, dateTimePicker y boton, se crean carpetas para almacenar los formularios y los ordenamientos, en los ordenamientos hicimos una clase llamada asi donde esta el metodo burbuja.

--- Calculadora/Formularios/frmBurbuja.cs
+++ Calculadora/Formularios/frmBurbuja.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Calculadora.Clases;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,11 +18,21 @@ namespace Calculadora
 
         private void btnOrdenar_Click(object sender, EventArgs e)
         {
+            Ordenamientos ordenamiento = new Ordenamientos();
             int[] ordenado = new int [dgvDesordenado.RowCount - 1];
 
-            for (int i = 0; i < dgvDesordenado.RowCount; i++)
+            for (int i = 0; i < dgvDesordenado.RowCount - 1; i++)
             {
                 ordenado[i] = Convert.ToInt32(dgvDesordenado.Rows[i].Cells[0].Value);
+            }
+
+            ordenado = ordenamiento.Burbuja(ordenado);
+
+            dgvOrdenado.DataSource = ordenado; //Define el tamaño de las filas del dgv de acuerdo al tamaño del arreglo
+
+            for(int i = 0; i < ordenado.Length; i++) 
+            {
+                dgvOrdenado.Rows[i].Cells[0].Value = ordenado[i]; //Inserta los datos ya ordenados
             }
         }
     }
---
+++ Calculadora/Formularios/frmRegistro.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Calculadora.Formularios
+{
+    partial class frmRegistro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            label1 = new Label();
+            txtNombre = new TextBox();
+            label2 = new Label();
+            txtApellido = new TextBox();
+            label3 = new Label();
+            dtpFecha = new DateTimePicker();
+            btnRegistrar = new Button();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(-3, 2);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(809, 450);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(btnRegistrar);
+            tabPage1.Controls.Add(dtpFecha);
+            tabPage1.Controls.Add(label3);
+            tabPage1.Controls.Add(txtApellido);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Controls.Add(txtNombre);
+            tabPage1.Controls.Add(label1);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(801, 422);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Registro";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(801, 422);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Mostrar";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(11, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(54, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Nombre:";
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(11, 46);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(342, 23);
+            txtNombre.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(11, 121);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Apellidos:";
+            // 
+            // txtApellido
+            // 
+            txtApellido.Location = new Point(11, 139);
+            txtApellido.Name = "txtApellido";
+            txtApellido.Size = new Size(342, 23);
+            txtApellido.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(11, 201);
+            label3.Name = "label3";
+            label3.Size = new Size(122, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Fecha de Nacimiento:";
+            // 
+            // dtpFecha
+            // 
+            dtpFecha.Location = new Point(11, 233);
+            dtpFecha.Name = "dtpFecha";
+            dtpFecha.Size = new Size(237, 23);
+            dtpFecha.TabIndex = 5;
+            // 
+            // btnRegistrar
+            // 
+            btnRegistrar.Location = new Point(712, 389);
+            btnRegistrar.Name = "btnRegistrar";
+            btnRegistrar.Size = new Size(75, 23);
+            btnRegistrar.TabIndex = 6;
+            btnRegistrar.Text = "Registrar";
+            btnRegistrar.UseVisualStyleBackColor = true;
+            // 
+            // frmRegistro
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tabControl1);
+            Name = "frmRegistro";
+            Text = "Registro Alumnos";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TextBox txtNombre;
+        private Label label1;
+        private TabPage tabPage2;
+        private Button btnRegistrar;
+        private DateTimePicker dtpFecha;
+        private Label label3;
+        private TextBox txtApellido;
+        private Label label2;
+    }
+}
